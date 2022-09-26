b25d1a98f4852b307c213f1e458f691d67af1eeb
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-09-26 21:00:51 +0200

    Map playlist items

--- GetYoutubePlaylist/index.ts
+++ GetYoutubePlaylist/index.ts
@@ -1,8 +1,6 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import got from "got";
 
-
-
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
@@ -11,23 +9,30 @@ const httpTrigger: AzureFunction = async function (
   const { YOUTUBE_API_KEY, YOUTUBE_PLAYLIST_ID } = process.env;
   const YOUTUBE_PLAYLIST_URL = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&key=${YOUTUBE_API_KEY}&playlistId=${YOUTUBE_PLAYLIST_ID}&maxResults=50`;
 
-  const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = name
-    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  //   const name = req.query.name || (req.body && req.body.name);
+  //   const responseMessage = name
+  //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+  //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
   try {
-    // const userId = await getUserId();
-    // if (!userId) {
-    //   throw Error(`no user id found for ${TWITTER_USER_NAME}`);
-    // }
     const playlist = await got<any>(YOUTUBE_PLAYLIST_URL, {
       responseType: "json",
     });
-    context.log(playlist.body);
-    // context.log(playlist.body.items.slice(0, 3));
+
+    const items = playlist.body.items.map((item) => ({
+      id: item.contentDetails.videoId,
+      publishedAt: new Date(item.contentDetails.videoPublishedAt),
+      title: item.snippet.title,
+      description: item.snippet.description
+        ? item.snippet.description.split("\n")
+        : [],
+      thumbnails: item.snippet.thumbnails,
+    }));
+
+    // context.log(playlist.body.items);
+
     context.res = {
-      body: `playlist`,
+      body: items,
     };
     return;
   } catch (err) {
@@ -37,11 +42,6 @@ const httpTrigger: AzureFunction = async function (
       body: "Invalid downstream response",
     };
   }
-
-  //   context.res = {
-  //     // status: 200, /* Defaults to 200 */
-  //     body: responseMessage,
-  //   };
 };
 
 export default httpTrigger;
